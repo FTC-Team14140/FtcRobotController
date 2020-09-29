332c142d2843d6a18391e252105d6c6cbf3ca736
devinbreise <1loverobots!>
2020-09-28 17:41:20 -0700

    added some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestDCMotorEx.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestDCMotorEx.java
@@ -4,6 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.basicLibs.teamUtil;
 
 
 @TeleOp(name="TestDCMotorEx")
@@ -22,17 +26,23 @@ public class TestDCMotorEx extends LinearOpMode{
         motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MotorConfigurationType m1Config = motor1.getMotorType();
+        MotorConfigurationType m2Config = motor1.getMotorType();
 
+        teamUtil.log("ticks" + motor1.getMotorType().getTicksPerRev());
         waitForStart();
 
         while(opModeIsActive() && !this.isStopRequested()){
             if (gamepad1.a) {
-                motor1.setVelocity(500);
-                motor2.setVelocity(500);
+                motor1.setVelocity(360, AngleUnit.DEGREES);
+                motor2.setVelocity(360, AngleUnit.DEGREES);
             } else {
                 motor1.setVelocity(0);
                 motor2.setVelocity(0);
             }
+            teamUtil.log("encoder:"+motor1.getCurrentPosition());
         }
 
     }
