5cd6c448e9673f0803c2a5787d444cd3fb4a919e
devinbreise <1loverobots!>
2020-10-09 13:26:06 -0700

    Modified for two motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestTubingIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestTubingIntake.java
@@ -6,10 +6,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="TestTubingIntake")
 public class TestTubingIntake extends LinearOpMode {
+    DcMotor theMotor2;
 
     DcMotor theMotor;
-    double FULL_POWER = 1.0;
-    double HALF_POWER = .5;
+    double FULL_POWER = .8;
+    double HALF_POWER = .4;
     double SLOW = .2;
 
 
@@ -17,19 +18,24 @@ public class TestTubingIntake extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         theMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        theMotor2 = hardwareMap.get(DcMotor.class, "intakeMotor2");
         waitForStart();
 
         while(opModeIsActive() && !this.isStopRequested()){
 
             if (gamepad1.y) {
                 theMotor.setPower(FULL_POWER);
+                theMotor2.setPower(-HALF_POWER);
             } else if (gamepad1.b) {
                 theMotor.setPower(HALF_POWER);
+                theMotor2.setPower(-HALF_POWER);
             } else if (gamepad1.a) {
                 theMotor.setPower(SLOW);
+                theMotor2.setPower(-SLOW);
             }
             if (gamepad1.x) {
                 theMotor.setPower(0);
+                theMotor2.setPower(0);
             }
         }
     }
