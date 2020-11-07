50cf0f8c5bda998ae4d64cd6371900a70772f3c2
devinbreise <1loverobots!>
2020-11-07 09:58:21 -0800

    test holonomic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestCode/TestShooter.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="TestShooter")
 public class TestShooter extends LinearOpMode {
     DcMotor m1, m2;
-    double FULL_POWER = .9;
-    double HALF_POWER = .4;
+    double FULL_POWER = 1;
+    double HALF_POWER = .8;
     double SLOW = .2;
 
 
@@ -23,6 +23,7 @@ public class TestShooter extends LinearOpMode {
 
 
             // Code for the 2 wheel shooter
+            /*
             if (gamepad1.y) {
                 m1.setPower(FULL_POWER);
                 m2.setPower(-HALF_POWER);
@@ -37,40 +38,59 @@ public class TestShooter extends LinearOpMode {
                 m1.setPower(0);
                 m2.setPower(0);
             }
-
+*/
             
 
-            /*
+
             //Code for the 1 wheel shooter
+/*
             if (gamepad1.y) {
-                m1.setPower(FULL_POWER);
+                m1.setPower(1);
             } else if (gamepad1.b) {
-                m1.setPower(HALF_POWER);
+                m1.setPower(0.9);
             } else if (gamepad1.a) {
-                m1.setPower(SLOW);
+                m1.setPower(0.8);
+            } else if (gamepad1.dpad_up) {
+                m1.setPower(0.7);
+            } else if (gamepad1.dpad_left) {
+                m1.setPower(0.6);
+            }   else if (gamepad1.dpad_down) {
+                m1.setPower(0.5);
             }
             if (gamepad1.x) {
                 m1.setPower(0);
             }
-            */
+*/
 
-            /*
             //Code for the 2 stage shooter
             if (gamepad1.y) {
-                m1.setPower(-FULL_POWER);
-                m2.setPower(FULL_POWER);
+                m1.setPower(-1);
+                //m2.setPower(1);
             } else if (gamepad1.b) {
-                m1.setPower(-HALF_POWER);
-                m2.setPower(HALF_POWER);
+                m1.setPower(-0.9);
+               // m2.setPower(1);
             } else if (gamepad1.a) {
-                m1.setPower(-SLOW);
-                m2.setPower(SLOW);
+                m1.setPower(-0.8);
+               // m2.setPower(1);
+            } else if (gamepad1.dpad_up) {
+                m1.setPower(-0.7);
+               //// m2.setPower(1);
+            } else if (gamepad1.dpad_left) {
+                m1.setPower(-0.6);
+             //   m2.setPower(1);
+            }   else if (gamepad1.dpad_down) {
+                m1.setPower(-0.5);
+              //  m2.setPower(1);
             }
+
+
             if (gamepad1.x) {
                 m1.setPower(0);
                 m2.setPower(0);
             }
-*/
+            if (gamepad1.dpad_right) {
+                m2.setPower(0);
+            }
         }
     }
 }
