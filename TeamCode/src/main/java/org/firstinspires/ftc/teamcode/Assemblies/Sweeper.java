17758d91f8a108dd44ce1c3bf5d42cd06f028a7b
ryoon <ryoon0510@gmail.com>
2021-01-02 12:01:44 -0800

    reset extend retract stop code added for the sweeper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Sweeper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/Sweeper.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Assemblies;
 
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -13,6 +14,13 @@ public class Sweeper {
     Telemetry telemetry;
     DcMotorEx motor;
     Servo sweeper;
+    Boolean sweeperCalibrated = false;
+    Double TENSION_POWER = 0.098;
+    int FULLY_EXTENDED = 1000; //TODO: find right number for extended encoder position
+    Double EXTEND_SPEED = 1000.0; //TODO: find right number
+    int FULLY_RETRACTED = 1000; //TODO: find right number for retracted encoder position
+    Double RETRACT_SPEED = 1000.0; //TODO: find right number
+
 
     void Sweeper() {
         teamUtil.log("Constructing Sweeper");
@@ -29,22 +37,45 @@ public class Sweeper {
     // retracts the arm fully and resets the encoder position to 0
     void reset() {
         teamUtil.log("Resetting Sweeper");
+        moveToStow();
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setPower(TENSION_POWER);
+        teamUtil.pause(500); // let them get going
+        do {
+            long last = motor.getCurrentPosition();
+            teamUtil.sleep(250);
+            // if things aren't moving, we have stalled
+            if (motor.getCurrentPosition() == last) {
+                motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motor.setPower(0);
+                motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                sweeperCalibrated = true;
+                teamUtil.log("Calibrating Sweeper - Finished");
 
+                return;
+            }
+            teamUtil.log("Sweeper Encoder:" + " " + last);
+
+        } while (true);
     }
 
     // Starts the blocker moving out at full speed.  Will continue until stop is called
     void extend() {
-
+        motor.setTargetPosition(FULLY_EXTENDED);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setVelocity(EXTEND_SPEED);
     }
 
     // Starts the blocker moving in and full speed.  Will continue until stop is called
     void retract() {
-
+        motor.setTargetPosition(FULLY_RETRACTED);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setVelocity(RETRACT_SPEED);
     }
 
     // Stop the blocker at its current position
     void stop() {
-
+        motor.setVelocity(0);
     }
 
     // Move the sweeper servo to the stow position
@@ -52,12 +83,12 @@ public class Sweeper {
 
     }
 
-    // Move the sweeper servo to the ready position (high enough to not block rings, but as close as possible.
+    // Move the sweeper servo to the ready position (run to position) (high enough to not block rings, but as close as possible.
     void moveToReady() {
 
     }
 
-    // Move the sweeper servo to the sweep position.
+    // Move the sweeper servo to the sweep position. (run to position)
     void moveToSweep() {
 
     }
