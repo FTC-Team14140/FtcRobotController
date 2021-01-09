4046730208d1dc839caa3eb426113a6aed2c0d19
lizzieyoon <lizzieyoon@icloud.com>
2021-01-09 11:14:26 -0800

    V2 grabber arm class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/GrabberArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Assemblies/GrabberArm.java
@@ -14,12 +14,21 @@ public class GrabberArm {
     Servo grabber;
 
     final double STALL_POWER = .098; //idk if this is the right number
+    final int HORIZONTAL = 1; // TODO: find correct number
+    final int STOW_POS = 1; // TODO: find correct number
+    final int READY_POS = 1; // TODO: find correct number
+    final int TRANSPORT_POS = 1; // TODO: find correct number
+    final double ARM_SPEED = .50; // TODO: find correct number
+    final float GRABBER_GRAB = .50f; // TODO: find correct number
+    final float GRABBER_OPEN = .50f; // TODO: find correct number
     boolean isReset = false;
 
     enum GRABBER_POS {
         OPEN,
         CLOSE
     }
+
+    GRABBER_POS currentGrabberPosition;
 
     void GrabberArm() {
         teamUtil.log("Constructing GrabberArm");
@@ -60,43 +69,51 @@ public class GrabberArm {
         } while (true);
     }
 
-    // Starts the blocker moving out at full speed.  Will continue until stop is called
-    void extend() {
-        arm.setTargetPosition(FULLY_EXTENDED);
-        arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        arm.setVelocity(EXTEND_SPEED);
-    }
-
-    // move the servo to the grab position
+    // move the servo to the grab positionu
     // This is also the stowed position
     void grab() {
-
+        grabber.setPosition(GRABBER_GRAB);
+        currentGrabberPosition = GRABBER_POS.CLOSE;
     }
 
     // move the servo to the release position
     // This is also the ready position
     void release() {
-
+        grabber.setPosition(GRABBER_OPEN);
+        currentGrabberPosition = GRABBER_POS.OPEN;
     }
 
     // Launches a new thread to move the arm to its ready position with the grabber open and ready
     void moveToReadyNoWait() {
-
+        placeAndRelease();
     }
 
     // Moves the arm to the place/ready position and releases the wobble goal.
     void placeAndRelease() {
-
+        arm.setTargetPosition(READY_POS);
+        arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        arm.setVelocity(ARM_SPEED);
+        while (Math.abs(arm.getCurrentPosition()-READY_POS)>50){
+        }
+        release();
     }
 
     // Launches a new thread to Grab a wobble goal and lift it up for transport
     void grabAndLiftNoWait () {
-
+        grab();
+        teamUtil.pause(1000);
+        arm.setTargetPosition(TRANSPORT_POS);
+        arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        arm.setVelocity(ARM_SPEED);
     }
 
     // stows the grabber and arm inside the robot
     void stow() {
-
+        grab();
+        teamUtil.pause(1000);
+        arm.setTargetPosition(STOW_POS);
+        arm.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        arm.setVelocity(ARM_SPEED);
     }
 
     // Launches a new thread to stow the grabber and arm inside the robot
